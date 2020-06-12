6e2327f8956f36dae2e40abeefc738911dd27132
eduard <eduard.shklyaruk.borusovuch@gmail.com>
2020-06-12 11:25:21 +0300

    [] Документація

--- src/components/Settings/Elements/Button.jsx
+++ src/components/Settings/Elements/Button.jsx
@@ -1,13 +1,10 @@
 import React, { useContext } from 'react'
-import './Button.css'
 import { BtnTheme } from '../BtnTheme';
+import './Button.css'
 
 function Button () {
-
     const theme = useContext(BtnTheme);
-
     const classButton = (theme === 'dark') ? 'btn--dark' : 'btn--ligth'
-
     return (
         <>
             <button className={`${classButton} btn`}>Context test</button>
--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import Button from './Elements/Button'
 import './Settings.css'
-import { BtnTheme } from './BtnTheme'
 
 const Settings = () => {
     return (
         <>
             <span className="title">Settings</span>
-            {/* <BtnTheme.Provider value="dark"> */}
-                <Button />
-            {/* </BtnTheme.Provider> */}
+            <Button />
         </>
     )
 }
